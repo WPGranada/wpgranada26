8071e7af401ebe075d1b2a7dbcbddd19187efccf
Alejandro Narvaez <alexnarvaez326@gmail.com>
2026-05-07 16:41:31 +0200

    Actualizar tema WPGranada completo

--- functions.php
+++ functions.php
@@ -68,4 +68,14 @@ function wpgranada26_enqueue_styles() {
 		array(),
 		wp_get_theme()->get( 'Version' )
 	);
+	function wpgranada_enqueue_styles() {
+    wp_enqueue_style(
+        'wpgranada-style',
+        get_stylesheet_uri(),
+        array(),
+        filemtime(get_stylesheet_directory() . '/style.css')
+    );
+}
+add_action('wp_enqueue_scripts', 'wpgranada_enqueue_styles');
+
 }
