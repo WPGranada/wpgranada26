89c6155cbfc04e07e1649a0ef258484f3e29e8de
castellon <alexcastellon077@gmail.com>
2026-06-29 11:04:19 +0200

    fix pattern home hero

--- patterns/hero-home.php
+++ patterns/hero-home.php
@@ -8,8 +8,8 @@
  */
 ?>
 <!-- wp:group {"metadata":{"categories":["wpgranada"],"patternName":"wpgranada26/hero-home","name":"Hero Home"},"align":"full","className":"hero-home-section","style":{"spacing":{"padding":{"top":"var:preset|spacing|90","bottom":"var:preset|spacing|90","left":"var:preset|spacing|80","right":"var:preset|spacing|80"}}},"backgroundColor":"base"} -->
-<div class="wp-block-group alignfull hero-home-section has-base-background-color has-background" style="padding-top:var(--wp--preset--spacing--90);padding-right:var(--wp--preset--spacing--80);padding-bottom:var(--wp--preset--spacing--90);padding-left:var(--wp--preset--spacing--80)"><!-- wp:columns {"isStackedOnMobile":false,"verticalAlignment":"center","className":"alignwide","style":{"spacing":{"blockGap":{"left":"var:preset|spacing|80"}}}} -->
-<div class="wp-block-columns is-not-stacked-on-mobile are-vertically-aligned-center alignwide"><!-- wp:column {"verticalAlignment":"center","width":"55%"} -->
+<div class="wp-block-group alignfull hero-home-section has-base-background-color has-background" style="padding-top:var(--wp--preset--spacing--90);padding-right:var(--wp--preset--spacing--80);padding-bottom:var(--wp--preset--spacing--90);padding-left:var(--wp--preset--spacing--80)"><!-- wp:columns {"verticalAlignment":"center","isStackedOnMobile":false,"className":"alignwide","style":{"spacing":{"blockGap":{"left":"var:preset|spacing|80"}}}} -->
+<div class="wp-block-columns are-vertically-aligned-center is-not-stacked-on-mobile alignwide"><!-- wp:column {"verticalAlignment":"center","width":"55%"} -->
 <div class="wp-block-column is-vertically-aligned-center" style="flex-basis:55%"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|50"}},"layout":{"type":"default"}} -->
 <div class="wp-block-group"><!-- wp:paragraph {"style":{"typography":{"fontWeight":"700","textTransform":"uppercase","letterSpacing":"0.12em"}},"textColor":"primary","fontSize":"x-small","fontFamily":"body"} -->
 <p class="has-primary-color has-text-color has-body-font-family has-x-small-font-size" style="font-weight:700;letter-spacing:0.12em;text-transform:uppercase">Comunidad Local · Open Web · Granada</p>
@@ -23,27 +23,27 @@
 <p class="has-primary-color has-text-color has-heading-font-family has-xx-large-font-size" style="font-weight:700;line-height:1.2">Aprende WordPress.<br>Comparte experiencia.</p>
 <!-- /wp:paragraph -->
 
-<!-- wp:paragraph {"style":{"typography":{"lineHeight":"1.6"}},"textColor":"tertiary","fontSize":"medium","fontFamily":"body"} -->
-<p class="has-tertiary-color has-text-color has-body-font-family has-medium-font-size" style="line-height:1.6">Una comunidad abierta de personas que quieren aprender, crear proyectos web y compartir conocimiento sobre WordPress: diseño, desarrollo, negocios y tecnología.</p>
+<!-- wp:paragraph {"style":{"typography":{"lineHeight":"1.6"}},"fontSize":"medium","fontFamily":"body"} -->
+<p class="has-body-font-family has-medium-font-size" style="line-height:1.6">Una comunidad abierta de personas que quieren aprender, crear proyectos web y compartir conocimiento sobre WordPress: diseño, desarrollo, negocios y tecnología.</p>
 <!-- /wp:paragraph -->
 
 <!-- wp:buttons {"style":{"spacing":{"blockGap":"var:preset|spacing|40"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
 <div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"accent","textColor":"base","style":{"border":{"radius":"100px"},"spacing":{"padding":{"top":"0.875rem","bottom":"0.875rem","left":"2rem","right":"2rem"}}},"fontSize":"medium","fontFamily":"body"} -->
-<div class="wp-block-button has-custom-font-size has-body-font-family has-medium-font-size"><a class="wp-block-button__link has-base-color has-accent-background-color has-text-color has-background wp-element-button" href="https://www.meetup.com/es-ES/wordpress-granada/" style="border-radius:100px;padding-top:0.875rem;padding-right:2rem;padding-bottom:0.875rem;padding-left:2rem">Únete a las Meetup</a></div>
+<div class="wp-block-button"><a class="wp-block-button__link has-base-color has-accent-background-color has-text-color has-background has-body-font-family has-medium-font-size has-custom-font-size wp-element-button" href="https://www.meetup.com/es-ES/wordpress-granada/" style="border-radius:100px;padding-top:0.875rem;padding-right:2rem;padding-bottom:0.875rem;padding-left:2rem">Únete a las Meetup</a></div>
 <!-- /wp:button -->
 
-<!-- wp:button {"className":"is-style-outline","style":{"border":{"radius":"100px"},"spacing":{"padding":{"top":"0.875rem","bottom":"0.875rem","left":"2rem","right":"2rem"}}},"fontSize":"medium","fontFamily":"body"} -->
-<div class="wp-block-button is-style-outline has-custom-font-size has-body-font-family has-medium-font-size"><a class="wp-block-button__link wp-element-button" href="/actividades" style="border-radius:100px;padding-top:0.875rem;padding-right:2rem;padding-bottom:0.875rem;padding-left:2rem">Ver actividades</a></div>
+<!-- wp:button {"backgroundColor":"primary","textColor":"base","style":{"border":{"radius":"100px"},"spacing":{"padding":{"top":"0.875rem","bottom":"0.875rem","left":"2rem","right":"2rem"}}},"fontSize":"medium","fontFamily":"body"} -->
+<div class="wp-block-button"><a class="wp-block-button__link has-base-color has-primary-background-color has-text-color has-background has-body-font-family has-medium-font-size has-custom-font-size wp-element-button" href="/actividades" style="border-radius:100px;padding-top:0.875rem;padding-right:2rem;padding-bottom:0.875rem;padding-left:2rem">Ver actividades</a></div>
 <!-- /wp:button --></div>
 <!-- /wp:buttons --></div>
 <!-- /wp:group --></div>
 <!-- /wp:column -->
 
 <!-- wp:column {"verticalAlignment":"center","width":"45%"} -->
-<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:45%"><!-- wp:query {"queryId":20,"query":{"perPage":1,"pages":0,"offset":0,"postType":"meetings","order":"asc","orderBy":"date","inherit":false},"layout":{"type":"default"}} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:45%"><!-- wp:query {"queryId":20,"query":{"perPage":1,"pages":0,"offset":0,"postType":"metgs_meeting","order":"desc","orderBy":"date","inherit":false,"sticky":"","parents":[],"format":[]},"layout":{"type":"default"}} -->
 <div class="wp-block-query"><!-- wp:post-template {"layout":{"type":"default"}} -->
 <!-- wp:group {"className":"hero-event-card","style":{"spacing":{"blockGap":"0"},"border":{"radius":"16px","color":"var(--wp--preset--color--primary-muted)","width":"1px"}},"backgroundColor":"base"} -->
-<div class="wp-block-group hero-event-card has-base-background-color has-background has-border-color" style="border-color:var(--wp--preset--color--primary-muted);border-width:1px;border-radius:16px"><!-- wp:post-featured-image {"isLink":false,"aspectRatio":"16/9","style":{"border":{"radius":"16px 16px 0px 0px"}}} /-->
+<div class="wp-block-group hero-event-card has-border-color has-base-background-color has-background" style="border-color:var(--wp--preset--color--primary-muted);border-width:1px;border-radius:16px"><!-- wp:post-featured-image {"aspectRatio":"16/9","style":{"border":{"radius":"16px 16px 0px 0px"}}} /-->
 
 <!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60","left":"var:preset|spacing|60","right":"var:preset|spacing|60"},"blockGap":"var:preset|spacing|40"}},"backgroundColor":"primary-light","layout":{"type":"default"}} -->
 <div class="wp-block-group has-primary-light-background-color has-background" style="padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--60)"><!-- wp:group {"style":{"spacing":{"blockGap":"0.35em"}},"layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"center"}} -->
@@ -51,10 +51,10 @@
 <p class="has-primary-color has-text-color has-body-font-family has-x-small-font-size" style="font-weight:700;letter-spacing:0.1em;text-transform:uppercase">Próxima Meetup ·</p>
 <!-- /wp:paragraph -->
 
-<!-- wp:post-date {"format":"j M","style":{"typography":{"fontWeight":"700","textTransform":"uppercase","letterSpacing":"0.1em"}},"textColor":"primary","fontSize":"x-small","fontFamily":"body"} /--></div>
+<!-- wp:post-date {"format":"j M","metadata":{"bindings":{"datetime":{"source":"core/post-data","args":{"field":"date"}}}},"style":{"typography":{"fontWeight":"700","textTransform":"uppercase","letterSpacing":"0.1em"}},"textColor":"primary","fontSize":"x-small","fontFamily":"body"} /--></div>
 <!-- /wp:group -->
 
-<!-- wp:post-title {"level":3,"isLink":false,"style":{"typography":{"fontWeight":"700","lineHeight":"1.2"}},"fontSize":"x-large","fontFamily":"heading"} /-->
+<!-- wp:post-title {"level":3,"style":{"typography":{"fontWeight":"700","lineHeight":"1.2"}},"fontSize":"x-large","fontFamily":"heading"} /-->
 
 <!-- wp:post-excerpt {"moreText":"","excerptLength":25,"style":{"typography":{"lineHeight":"1.5"}},"textColor":"tertiary","fontSize":"small","fontFamily":"body"} /-->
 
